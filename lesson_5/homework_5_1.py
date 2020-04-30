7e089c805dd072e4f59ee6e0574a1c3f9dae9f4a
cosmostate <tsverdakhleb@gmail.com>
2020-04-30 18:34:11 -0400

    completed hw 5.1

--- lesson_5/homework_5_1.py
+++ lesson_5/homework_5_1.py
@@ -3,32 +3,49 @@
 # Enter your name, save it in name variable and create function print_name_three_times which returns
 # value that is equal to your name three times
 
-name_1 = None
+name_1 = 'Ulyana'
 
 def print_name_three_times(name):
-    pass
+    return name * 3
+
+result_1 = print_name_three_times(name_1)
+print(result_1)
 
 
 # Modify your previous program so that it will enter your name (save it in variable  name_2) and a number
 # (save in variable number) and then display their name that number of times. Each time add your name to result
 # variable
 
-name_2 = None
-number_1 = None
+name_2 = 'Ulyana'
+number_1 = 3
 
 def print_name_number_times(number, name):
-    pass
+    return name * number
 
+result_2 = print_name_number_times(number_1, name_2)
+print(result_2)
 
 # Enter a random string, which includes only digits. Write function sum_digits which find a sum of digits in this string
 
-string_number_1 = None
+string_number_1 = '482901645'
 
 def sum_digits(string):
-    pass
+    result = 0
+    for char in string:
+        result += int(char)
+    return result
+
+result_3 = sum_digits(string_number_1)
+print(result_3)
 
 
 # Create function which sums up all even numbers between 2 and 100 (include 100)
 
 def sum_even_numbers():
-    pass
+    result = 0
+    for number in range(2, 101, 2):
+        result += number
+    return result
+
+result_4 = sum_even_numbers()
+print(result_4)
